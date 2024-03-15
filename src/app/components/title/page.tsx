06328b4cf0fd13b9a0c73e2ee0d71442805ec47b
Petani Kode <rezzabagus.rb@gmail.com>
2024-03-15 16:20:03 +0700

    fix: fixing prod titleProps 3

--- src/app/components/title/page.tsx
+++ src/app/components/title/page.tsx
@@ -1,11 +1,12 @@
 // import { useState } from "react";
 
 interface TitleProps {
-    [title: string]: any;
+    title: string;
     darkText: string;
+    [key: string]: any;
 }
 
-export default function Title({ title, darkText }: TitleProps) {
+export default function Title({ title, darkText, ...rest }: TitleProps) {
     // const [data, setData] = useState<TitleProps[]>([])
     return <p className={`dark:text-white ${darkText} text-5xl font-extrabold text-darkColor500 col-span-1 mb-4 lg:mb-0`}>{title}</p>
 }
