b1e3e6431a79128c93e01fe74fff0273117de5be
Petani Kode <rezzabagus.rb@gmail.com>
2024-03-15 15:58:46 +0700

    fix: fixing prod titleProps

--- src/app/components/title/page.tsx
+++ src/app/components/title/page.tsx
@@ -1,8 +1,8 @@
 interface TitleProps {
-    name: string;
+    title: string;
     darkText: string;
 }
 
-export default function Title({ name, darkText }: TitleProps) {
-    return <p className={`dark:text-white ${darkText} text-5xl font-extrabold text-darkColor500 col-span-1 mb-4 lg:mb-0`}>{name}</p>
+export default function Title({ title, darkText }: TitleProps) {
+    return <p className={`dark:text-white ${darkText} text-5xl font-extrabold text-darkColor500 col-span-1 mb-4 lg:mb-0`}>{title}</p>
 }
