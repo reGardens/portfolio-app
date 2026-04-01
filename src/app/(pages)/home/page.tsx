c7aea68938a6acb337558d203109474e3f1c05af
rezabaguspratama <reza.pratama@pcsindonesia.co.id>
2026-04-01 22:02:41 +0700

    aa

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -1,27 +1,22 @@
 import type { Metadata } from 'next';
-import { promises as fs } from 'fs';
 import HomeClientSide from './HomeClientSide';
+import dataIcons from '../../../../public/static/dataSkills.json';
+import dataProjects from '../../../../public/static/dataProjects.json';
 
 export const metadata: Metadata = {
     title: 'Home',
     description: 'Home Page',
 }
 
-export const dynamic = 'force-dynamic';
-
 interface Item {
     name: string,
 }
 
 export default async function Home() {
-    const icons = await fs.readFile(process.cwd() + '/public/static/dataSkills.json', 'utf8');
-    const dataIcons = await JSON.parse(icons);
     const dataFrontEnd = dataIcons.find((item: Item) => item.name === 'frontend');
     const dataBackEnd = dataIcons.find((item: Item) => item.name === 'backend');
     const dataOtherSkills = dataIcons.find((item: Item) => item.name === 'other');
 
-    const projects = await fs.readFile(process.cwd() + '/public/static/dataProjects.json', 'utf8');
-    const dataProjects = await JSON.parse(projects);
     const latestProject = dataProjects.slice(-3);
 
     return (
--- src/app/(pages)/project/page.tsx
+++ src/app/(pages)/project/page.tsx
@@ -1,27 +1,21 @@
 import type { Metadata } from 'next';
-import { promises as fs } from 'fs';
 import DetailProjects from './detailProjects';
+import dataIcons from '../../../../public/static/dataSkills.json';
+import dataProjects from '../../../../public/static/dataProjects.json';
 
 export const metadata: Metadata = {
     title: 'Detail Projects',
     description: 'Detail Project Page',
 }
 
-export const dynamic = 'force-dynamic';
-
 interface Item {
     name: string,
 }
 
 export default async function Project() {
-    const icons = await fs.readFile(process.cwd() + '/public/static/dataSkills.json', 'utf8');
-    const dataIcons = await JSON.parse(icons);
     const dataFrontEnd = dataIcons.find((item: Item) => item.name === 'frontend');
     const dataBackEnd = dataIcons.find((item: Item) => item.name === 'backend');
     const dataOtherSkills = dataIcons.find((item: Item) => item.name === 'other');
-
-    const projects = await fs.readFile(process.cwd() + '/public/static/dataProjects.json', 'utf8');
-    const dataProjects = await JSON.parse(projects);
 
     return (
         <>
